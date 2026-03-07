eecec24abbb47a8d12e0b443359110db7dc83f67
Nilesh Deb <debnilesh69@gmail.com>
2026-03-07 21:00:49 +0530

    Display Table Data in Django Template

---
+++ myProject9/portfolio/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from    .models import Student
+
+
+# Create your views here.
+def student_list(request):
+    students= Student.objects.all()
+    return render(request, 'portfolio/student_list.html',{'students': students})
