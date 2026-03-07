653f233b9c13a6e3fcd6e48fb444fa936c88f46b
Nilesh Deb <debnilesh69@gmail.com>
2026-03-07 22:39:02 +0530

    Register & Manage Models in Django Admin

--- myProject10/portfolio/admin.py
+++ myProject10/portfolio/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from portfolio.models import Student    
+from portfolio.models import Profile
+
+admin.site.register(Student)
+admin.site.register(Profile)
 
 # Register your models here.
