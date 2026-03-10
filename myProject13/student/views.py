bb1bddec0660195a1264e2aff78bb63d345027c6
Nilesh Deb <debnilesh69@gmail.com>
2026-03-10 12:14:33 +0530

    Django ModelForms | Read

--- myProject13/student/views.py
+++ myProject13/student/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from .forms import StudentForm
+from .models import Student
 
 def student_create(request):
     form = StudentForm()
@@ -9,3 +10,12 @@ def student_create(request):
             form.save()
             return render(request, "student_success.html")
     return render(request, "student_form.html", {"form": form})
+
+def student_list(request):
+    students=Student.objects.all()
+    return render(request, "student_list.html", {"students": students}) 
+
+def student_detail(request, pk):
+    student = get_object_or_404(Student, pk=pk)
+    return render(request, "student_detail.html", {"student": student})
+
