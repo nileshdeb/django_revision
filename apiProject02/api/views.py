f18875e720d2cc5daa7ebe1274d372a5c7d42761
Nilesh Deb <debnilesh69@gmail.com>
2026-03-25 21:34:04 +0530

    Django REST Framework | GET Request

---
+++ apiProject02/api/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from .models import Student
+from .serializers import StudentSerializer
+
+@api_view(['GET'])
+def get_students(request):
+    students = Student.objects.all()
+    serializer = StudentSerializer(students, many=True)
+    return Response(serializer.data)    
