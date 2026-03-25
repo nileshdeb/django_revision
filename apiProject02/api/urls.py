795e6f2585363245a90f426ef6487e5ba75cbbc4
Nilesh Deb <debnilesh69@gmail.com>
2026-03-25 22:21:11 +0530

    Django REST Framework | PUT & PATCH Request

--- apiProject02/api/urls.py
+++ apiProject02/api/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('students/', views.get_students, name='get_students'),
     path('students/add/', views.add_student, name='add_student'),
+    path('students/update/<int:pk>/', views.update_student, name='update_student'), 
 ]
--- apiProject02/api/views.py
+++ apiProject02/api/views.py
@@ -17,3 +17,21 @@ def add_student(request):
         serializer.save()
         return Response(serializer.data, status=201)
     return Response(serializer.errors, status=400)
+
+@api_view(['PUT','PATCH'])
+def update_student(request, pk):
+    try:
+        student = Student.objects.get(id=pk)
+    except Student.DoesNotExist:
+        return Response(status=404)
+    # partial update
+    if request.method == 'PATCH':
+        serializer = StudentSerializer(student, data=request.data, partial=True)
+    else:    
+        serializer = StudentSerializer(student, data=request.data)
+
+
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data)
+    return Response(serializer.errors, status=400)
