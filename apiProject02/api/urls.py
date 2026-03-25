74cbd0b7ade1626f639716c63a5796ce35dcd2e8
Nilesh Deb <debnilesh69@gmail.com>
2026-03-25 21:48:20 +0530

    Django REST Framework | POST Request

--- apiProject02/api/urls.py
+++ apiProject02/api/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('students/', views.get_students, name='get_students'),
+    path('students/add/', views.add_student, name='add_student'),
 ]
--- apiProject02/api/views.py
+++ apiProject02/api/views.py
@@ -9,3 +9,11 @@ def get_students(request):
     students = Student.objects.all()
     serializer = StudentSerializer(students, many=True)
     return Response(serializer.data)    
+
+@api_view(['POST'])
+def add_student(request):
+    serializer = StudentSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=201)
+    return Response(serializer.errors, status=400)
