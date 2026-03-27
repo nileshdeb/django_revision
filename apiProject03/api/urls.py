dcb9681ec6533a6a062aae8318628ae673d60101
Nilesh Deb <debnilesh69@gmail.com>
2026-03-27 11:28:15 +0530

    Django REST Framework CRUD using GenericAPIView + Mixins

--- apiProject03/api/urls.py
+++ apiProject03/api/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
-from .views import StudentAPI
+from .views import StudentListCreateAPI, StudentRetrieveUpdateDestroyAPI    
+# from .views import StudentAPI
+
+# urlpatterns = [
+#     path("students/", StudentAPI.as_view(), name="students"),
+#     path("students/<int:pk>/", StudentAPI.as_view(), name="student-detail"),
+# ]
 
 urlpatterns = [
-    path("students/", StudentAPI.as_view(), name="students"),
-    path("students/<int:pk>/", StudentAPI.as_view(), name="student-detail"),
+    path("students/", StudentListCreateAPI.as_view(), name="students"),
+    path("students/<int:pk>/", StudentRetrieveUpdateDestroyAPI.as_view(), name="student-detail"),
 ]
