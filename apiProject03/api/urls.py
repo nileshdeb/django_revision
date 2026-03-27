87acf485a35c31047aac34153321ac699112c712
Nilesh Deb <debnilesh69@gmail.com>
2026-03-27 11:49:05 +0530

    Django REST Framework ModelViewSet & Routers

--- apiProject03/api/urls.py
+++ apiProject03/api/urls.py
@@ -1,13 +1,22 @@
-from django.urls import path
-from .views import StudentListCreateAPI, StudentRetrieveUpdateDestroyAPI    
+# from django.urls import path
+# from .views import StudentListCreateAPI, StudentRetrieveUpdateDestroyAPI    
 # from .views import StudentAPI
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import StudentViewSet
+
+router=DefaultRouter()
+router.register('students', StudentViewSet, basename='student')
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
+
 
 # urlpatterns = [
 #     path("students/", StudentAPI.as_view(), name="students"),
 #     path("students/<int:pk>/", StudentAPI.as_view(), name="student-detail"),
 # ]
 
-urlpatterns = [
-    path("students/", StudentListCreateAPI.as_view(), name="students"),
-    path("students/<int:pk>/", StudentRetrieveUpdateDestroyAPI.as_view(), name="student-detail"),
-]
+
+
--- apiProject03/api/views.py
+++ apiProject03/api/views.py
@@ -1,42 +1,52 @@
-# from rest_framework.views import APIView
-# from rest_framework.response import Response
-# from rest_framework import status
+# # from rest_framework.views import APIView
+# # from rest_framework.response import Response
+# # from rest_framework import status
+# # from .models import Student
+# # from .serializers import StudentSerializer
+
+
+
+
+# from rest_framework import generics,mixins
 # from .models import Student
 # from .serializers import StudentSerializer
 
-
-
-
-from rest_framework import generics,mixins
+from rest_framework import viewsets
 from .models import Student
 from .serializers import StudentSerializer
 
-#crud operations using generic views and mixins
-class StudentListCreateAPI(generics.GenericAPIView,mixins.ListModelMixin,mixins.CreateModelMixin):
+
+# crud using viewsets
+class StudentViewSet(viewsets.ModelViewSet):
     queryset=Student.objects.all()
     serializer_class=StudentSerializer
 
-    # REad all data (get)
-    def get(self, request,*args, **kwargs):
-        return self.list(request,*args, **kwargs)
-    # Create data (post)
-    def post(self, request,*args, **kwargs):
-        return self.create(request,*args, **kwargs)
+# #crud operations using generic views and mixins
+# class StudentListCreateAPI(generics.GenericAPIView,mixins.ListModelMixin,mixins.CreateModelMixin):
+#     queryset=Student.objects.all()
+#     serializer_class=StudentSerializer
 
-class StudentRetrieveUpdateDestroyAPI(generics.GenericAPIView,mixins.RetrieveModelMixin,mixins.UpdateModelMixin,mixins.DestroyModelMixin):
-    queryset=Student.objects.all()
-    serializer_class=StudentSerializer
+#     # REad all data (get)
+#     def get(self, request,*args, **kwargs):
+#         return self.list(request,*args, **kwargs)
+#     # Create data (post)
+#     def post(self, request,*args, **kwargs):
+#         return self.create(request,*args, **kwargs)
 
-    # GEt single data (get)
-    def get(self, request,*args, **kwargs):
-        return self.retrieve(request,*args, **kwargs)
+# class StudentRetrieveUpdateDestroyAPI(generics.GenericAPIView,mixins.RetrieveModelMixin,mixins.UpdateModelMixin,mixins.DestroyModelMixin):
+#     queryset=Student.objects.all()
+#     serializer_class=StudentSerializer
+
+#     # GEt single data (get)
+#     def get(self, request,*args, **kwargs):
+#         return self.retrieve(request,*args, **kwargs)
     
-    # Update data (put)
-    def put(self, request,*args, **kwargs):
-        return self.update(request,*args, **kwargs)
-    # Delete data (delete)
-    def delete(self, request,*args, **kwargs):
-        return self.destroy(request,*args, **kwargs)
+#     # Update data (put)
+#     def put(self, request,*args, **kwargs):
+#         return self.update(request,*args, **kwargs)
+#     # Delete data (delete)
+#     def delete(self, request,*args, **kwargs):
+#         return self.destroy(request,*args, **kwargs)
 
 
 
@@ -86,45 +96,45 @@ class StudentRetrieveUpdateDestroyAPI(generics.GenericAPIView,mixins.RetrieveMod
 
 
 
-#  class StudentAPI(APIView):
-#     def get(self, request,pk=None):
-#        if pk:
-#         try:
-#             student=Student.objects.get(id=pk) 
-#             serializer=StudentSerializer(student)
-#             return Response(serializer.data,status=status.HTTP_200_OK)
-#         except Student.DoesNotExist:
-#             return Response({'message':'Student not found'},status=status.HTTP_404_NOT_FOUND)
-#        else:
-#         students=Student.objects.all()
-#         serializer=StudentSerializer(students,many=True)
-#         return Response(serializer.data,status=status.HTTP_200_OK)
+# #  class StudentAPI(APIView):
+# #     def get(self, request,pk=None):
+# #        if pk:
+# #         try:
+# #             student=Student.objects.get(id=pk) 
+# #             serializer=StudentSerializer(student)
+# #             return Response(serializer.data,status=status.HTTP_200_OK)
+# #         except Student.DoesNotExist:
+# #             return Response({'message':'Student not found'},status=status.HTTP_404_NOT_FOUND)
+# #        else:
+# #         students=Student.objects.all()
+# #         serializer=StudentSerializer(students,many=True)
+# #         return Response(serializer.data,status=status.HTTP_200_OK)
 
-# #Create data (post)
-#     def post(self, request):
-#         serializer=StudentSerializer(data=request.data)
-#         if serializer.is_valid():
-#             serializer.save()
-#             return Response(serializer.data,status=status.HTTP_201_CREATED)
-#         return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+# # #Create data (post)
+# #     def post(self, request):
+# #         serializer=StudentSerializer(data=request.data)
+# #         if serializer.is_valid():
+# #             serializer.save()
+# #             return Response(serializer.data,status=status.HTTP_201_CREATED)
+# #         return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
 
-# #Update data (put)
-#     def put(self, request,pk=None):
-#         try:
-#             student=Student.objects.get(id=pk) 
-#             serializer=StudentSerializer(student,data=request.data)
-#             if serializer.is_valid():
-#                 serializer.save()
-#                 return Response(serializer.data,status=status.HTTP_200_OK)
-#             return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
-#         except Student.DoesNotExist:
-#             return Response({'message':'Student not found'},status=status.HTTP_404_NOT_FOUND)
+# # #Update data (put)
+# #     def put(self, request,pk=None):
+# #         try:
+# #             student=Student.objects.get(id=pk) 
+# #             serializer=StudentSerializer(student,data=request.data)
+# #             if serializer.is_valid():
+# #                 serializer.save()
+# #                 return Response(serializer.data,status=status.HTTP_200_OK)
+# #             return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+# #         except Student.DoesNotExist:
+# #             return Response({'message':'Student not found'},status=status.HTTP_404_NOT_FOUND)
 
-# #Delete data (delete)
-#     def delete(self, request,pk=None):
-#         try:
-#             student=Student.objects.get(id=pk) 
-#             student.delete()
-#             return Response({'message':'Student deleted'},status=status.HTTP_200_OK)
-#         except Student.DoesNotExist:
-#             return Response({'message':'Student not found'},status=status.HTTP_404_NOT_FOUND)
+# # #Delete data (delete)
+# #     def delete(self, request,pk=None):
+# #         try:
+# #             student=Student.objects.get(id=pk) 
+# #             student.delete()
+# #             return Response({'message':'Student deleted'},status=status.HTTP_200_OK)
+# #         except Student.DoesNotExist:
+# #             return Response({'message':'Student not found'},status=status.HTTP_404_NOT_FOUND)
